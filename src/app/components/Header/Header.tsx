18d502d7fdde68365fb95e49866224f2b0d71deb
Jérôme DENIAU <jerome.deniau.simplon@gmail.com>
2023-06-23 19:49:40 +0200

    addSearchPage

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -14,7 +14,8 @@ const Header = () => {
     }
 
     const handleSearchSubmit = () => {
-        console.log(search); // Afficher la valeur de recherche dans la console
+        window.location.href = `/search/${search}`;
+
     }
 
     return (
@@ -38,30 +39,3 @@ const Header = () => {
 export default Header;
 
 
-
-// 'use client'
-
-// import styles from './albumcard.module.scss';
-// import Link from 'next/link';
-
-// interface AlbumCardProps {
-//   title: string;
-//   imageUrl: string;
-//   artistName: string;
-//   albumId: string;
-// }
-
-// const AlbumCard: React.FC<AlbumCardProps> = ({ title, imageUrl, artistName, albumId }) => {
-
-//   return (
-//     <Link href={`/album/${albumId}`} className={styles.albumCard}>
-//       <img src={imageUrl} alt={title} className={styles.albumImage} />
-//       <div className={styles.albumInfo}>
-//         <p className={styles.albumTitle}>{title}</p>
-//         <p className={styles.albumArtist}>{artistName}</p>
-//       </div>
-//     </Link>
-//   );
-// };
-
-// export default AlbumCard;
--- src/app/search/[id]/page.tsx
+++ src/app/search/[id]/page.tsx
@@ -1,5 +1,3 @@
-// Cette route pourrait être utilisée pour afficher les détails d'un album spécifique. Par exemple, /album/67890 afficherait les détails de l'album avec l'ID 67890.
-
 
 import styles from '../../page.module.scss';
 import fetchSpotifyApi from '../../spotifyAPI';
@@ -7,30 +5,33 @@ import AlbumCard from '../../components/AlbumCard/AlbumCard';
 
 
 
-// ${searchId}
-// ajouter async a la function si besoin d'un await
-const Search = async () => {
-  const thisSearch = await fetchSpotifyApi(`search?q=Damso&type=album%2Cplaylist%2Ctrack%2Cartist&market=FR&limit=50&offset=0`)
-console.log(thisSearch)
-return (
-    
+export default async function SearchPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const searchQuery = params.id;
+
+  
+  // Utilisez searchQuery pour faire votre appel API
+  const thisSearch = await fetchSpotifyApi(`search?q=${searchQuery}&type=album%2Cplaylist%2Ctrack%2Cartist&market=FR&limit=50&offset=0`);
+
+
+  return (
     <div className={styles.container}>
       <div className={styles.childcontainer}>
-      <div className={styles.cards}>
+        <div className={styles.cards}>
           {thisSearch?.albums.items.map((album: any) => (
             <AlbumCard
               title={album.name}
               imageUrl={album.images[0].url}
               artistName={album.artists[0].name}
-              albumId={album.id} // Utiliser album.id au lieu de albumId
+              albumId={album.id}
             />
           ))}
         </div>
       </div>
     </div>
-);
-
+  );
 };
 
-
-export default Search;
